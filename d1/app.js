259ba456161d3ad22cd894309a70d8aa44315e3d
eldinhasanovic <almirhasanovic40@gmail.com>
2023-03-07 13:05:50 +0100

    domaci

--- d1/app.js
+++ d1/app.js
@@ -7,8 +7,32 @@ class Kurs {
     this.duzina = duzina;
     this.cena = cena;
   }
+  mesec() {
+    return "Mesecna cena ovog kursa je " + this.cena / this.duzina;
+  }
+  opis() {
+    return `Kurs ${this.ime} traje ${this.duzina} mjeseca/i i kosta ${this.cena}$`;
+  }
 }
-const kurs1 = new Kurs("Dizajn", "3 meseca", 150);
-const kurs2 = new Kurs("ITCamp", "9 meseci", 350);
-console.log(kurs1);
-console.log(kurs2);
+class PracticalCourse extends Kurs {
+  numOfExercises;
+  constructor(ime, duzina, cena, numOfExercises) {
+    super(ime, duzina, cena);
+    this.numOfExercises = numOfExercises;
+  }
+}
+class TheoreticalCourse extends Kurs {
+  constructor(ime, duzina, cena) {
+    super(ime, duzina, cena);
+  }
+  publish() {
+    return "published";
+  }
+}
+const kurs1 = new Kurs("Dizajn", 3, 150);
+const kurs2 = new Kurs("ITCamp", 9, 350);
+console.log(kurs1.mesec());
+console.log(kurs2.opis());
+const kurs3 = new PracticalCourse("Prakticni kurs", 1, 100, 12);
+kurs3.dolari;
+console.log(kurs3);
