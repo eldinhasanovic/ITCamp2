b97da2183502d94e0e4434897daa16c7ba1ce3d1
eldinhasanovic <almirhasanovic40@gmail.com>
2023-03-11 16:18:36 +0100

    domaci2

--- 2/d2/app.js
+++ 2/d2/app.js
@@ -1,4 +1,22 @@
-const rec = "haajsdlkjioopqweopojksan";
-rec = rec.toLowerCase;
-for (i = 1; i < rec.length; i++) {}
-const obecanje = new Promise((resolve, reject) => {});
+// Napraviti promise koji proverava da li je neki objekat ispunio uslova da je njegov property godine veci od 17 ili nije.
+// U slucaju da je osoba punoletna then() funkcija vraca poruku "Vi ste punoletni".
+// U slucaju da osoba nije punoletna catch() vraca broj godina osobe.
+const person = {
+  Ime: "Eldin",
+  Prezime: "Hasanovic",
+  Godine: 16,
+};
+function punoletnost(godine) {
+  const promise = new Promise((resolve, reject) => {
+    if (godine > 17) {
+      resolve("VI ste punoletni");
+    } else {
+      reject(godine);
+    }
+  });
+
+  promise
+    .then((message) => console.log(message))
+    .catch((message) => console.log(message));
+}
+console.log(punoletnost(person.Godine));
