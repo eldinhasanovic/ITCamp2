102c98a18c3aac5e2d4f03fe0d7d42598853e625
eldinhasanovic <almirhasanovic40@gmail.com>
2023-03-16 21:03:56 +0100

    ..

---
+++ 4/app.js
@@ -0,0 +1,18 @@
+// const containers = document.getElementsByClassName("container");
+// const container = containers[0];
+// container.style.backgroundColor = "#f0f0f0";
+// container.style.border = "1px solid #f00";
+// const listItems = document.getElementsByTagName("li");
+
+// for (let i = 0; i < listItems.length; i++) {
+//   if (i === 1) {
+//     li;
+//   }
+// }
+// const listItems2 = document.querySelectorAll("li");
+// const evenLi = document.querySelectorAll("li:nth-child(even)");
+// const oddLi = document.querySelectorAll("li:nth-child(odd)");
+// for (i = 0; i < oddLi.length; i++) {
+//   oddLI[i].style.backgroundColor = "blue";
+//   evenLi[i].style.backgroundColor = "orange";
+// }
