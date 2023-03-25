7488eab3b0d98c4b25492ff97ab32d55cbf43cbf
eldinhasanovic <almirhasanovic40@gmail.com>
2023-03-25 16:45:36 +0100

    .

--- 5 pogodibr/app.js
+++ 5 pogodibr/app.js
@@ -1,28 +1,47 @@
 broj = document.querySelector("input");
 dugme = document.querySelector(".submit");
-pogodak = document.querySelector("h4");
+pogodak = document.querySelector(".h4");
 rndm = Math.round(Math.random() * 20);
+score = document.querySelector(".brojac");
 play = document.querySelector(".ponovo");
+skor = document.querySelector(".hs");
 console.log(rndm);
-function playagain() {
-  rndm = Math.round(Math.random() * 20);
-  console.log(rndm);
-  play.style.display = "none";
-}
+
 function submit() {
   br = broj.value;
-  //   if (br > 20) {
-  //     document.querySelector("h4").innerHTML = "uneti broj je veci od 20";
-  //   }
-  if (br < 1) {
+  if (br > 20) {
+    pogodak.innerHTML = "uneti broj je veci od 20";
+  } else if (br < 0) {
     pogodak.innerHTML = "uneti broj je manji od 1";
   } else if (br > rndm) {
     pogodak.innerHTML = "manje";
-    var brojac = 0;
   } else if (br < rndm) {
     pogodak.innerHTML = "vise";
   } else if (br == rndm) {
     pogodak.innerHTML = "pogodili ste  broj";
     play.style.display = "inline";
+    if (score.innerHTML > skor.innerHTML) {
+      skor.innerHTML = score.innerHTML;
+    }
   }
+  // else if (br == 21) {
+  //     pogodak.innerHTML = "niste uneli broj";
+  //   }
 }
+var count = 0;
+dugme.addEventListener("click", function () {
+  count++;
+  score.innerHTML = 20 - count;
+});
+function playagain() {
+  rndm = Math.round(Math.random() * 20);
+  console.log(rndm);
+  play.style.display = "none";
+  score.innerHTML = 20;
+}
+// broj.addEventListener("keypress", function (event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     document.getElementsByClassName("submit").click();
+//   }
+// });
